972d7f477f91fd97029d418ed602070054bf4676
ana <100451209@alumnos.uc3m.es>
2024-05-13 15:55:25 +0200

    .

---
+++ encoding.py
@@ -0,0 +1,27 @@
+import chardet
+import pandas as pd
+
+def detect_encoding(file_path):
+    with open(file_path, 'rb', ) as f:
+        raw_data = f.read()
+        result = chardet.detect(raw_data)
+        encoding = result['encoding']
+        confidence = result['confidence']
+        return encoding, confidence
+
+
+def convert_encoding(input_file, output_file, encoding):
+    with open(input_file, 'r', encoding=encoding) as f:
+        data = f.read()
+
+    with open(output_file, 'w', encoding='utf-8') as f:
+        f.write(data)
+
+def xls_to_csv(input_file):
+    archivo_xls = pd.read_excel(input_file)
+    output_file = input_file[:-4] + '.csv'
+    archivo_xls.iloc[8:].to_csv(output_file, index=False, sep=';')
+    return output_file
+
+
+
---
+++ main.py
@@ -0,0 +1,21 @@
+import os
+from clean import clean_folder, sector_sum
+
+# EJEMPLO
+input_folder = './original_data'
+
+for folder_name in os.listdir(input_folder):
+    # Combinar el nombre del directorio con la ruta base para obtener la ruta completa
+    folder_path = os.path.join(input_folder, folder_name)
+        # Tiene un encoding diferent
+
+    # Verificar si el elemento es un directorio
+    if os.path.isdir(folder_path):
+        # Aquí puedes hacer lo que necesites con el directorio
+        output_path = clean_folder(folder_path)
+        # output_path_2020 = clean_folder(folder_path, 2020, start)
+        if 'sector' in folder_path:
+            keywords= output_path.split('/')[-1][:-7]
+            sector_sum(keyword=keywords, folder=output_path)
+            # sector_sum(keyword=keywords, folder=output_path_2020)
+
